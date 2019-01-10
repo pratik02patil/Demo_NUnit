b9a830f6bb9501dad450baccf6227c7b74c64833
pratik02patil <45798451+pratik02patil@users.noreply.github.com>
2019-01-10 04:46:30 -0800

    demo project

---
+++ NunitTestDemo/TestClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NunitTestDemo
+{
+
+
+    [TestFixture]
+    class TestClass
+    {
+        [TestCase]
+
+        
+
+        public void Add()
+        {
+            myClass addition = new myClass();
+            Assert.AreEqual(30, addition.Add(10, 20));
+
+        }
+
+
+        [TestCase]
+        public void Sub()
+        {
+            myClass addition = new myClass();
+            Assert.AreEqual(10, addition.Sub(20, 10));
+
+        }
+
+
+
+    }
+}
---
+++ NunitTestDemo/myClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NunitTestDemo
+{
+    class myClass
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+
+        public int Sub(int a, int b)
+        {
+            return a - b;
+
+        }
+
+    }
+}
